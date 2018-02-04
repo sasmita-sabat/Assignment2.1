7612fe972f88734547be5f5c35a44dc904fdb650
sasmita-sabat <35617622+sasmita-sabat@users.noreply.github.com>
2018-02-04 20:33:22 +0530

    Add files via upload
    Assignment 1 session 2

---
+++ Assignment2.1/Session2_Assignment1.py
@@ -0,0 +1,13 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+#Program to input as comma separated numbers  generate it as list.
+s=input("Enter the sequence of comma separated numbers ")
+l=[]
+#split function would convert the string into a list.
+l=s.split(',')
+l
+
